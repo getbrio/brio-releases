20f09ab54eb3b1a67a3de62a60c83fae0e38ea67
Patrick Walsh <pw4.2020@gmail.com>
2026-05-07 11:37:31 -0400

    change docs site

--- brio-docs/docusaurus.config.ts
+++ brio-docs/docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  url: 'https://docs.getbrio.org',
+  url: 'https://docs.getbrio.dev',
   baseUrl: '/',
 
   onBrokenLinks: 'throw',
